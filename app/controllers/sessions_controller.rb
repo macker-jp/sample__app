fbfd21ff81bd2f64e2706cb87d961537cf531ac7
macker-jp <masaya.fujiki@icloud.com>
2025-03-13 07:35:18 +0000

    Add flash to log-in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # aaa
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
   end
+
+  def destroy; end
 end
