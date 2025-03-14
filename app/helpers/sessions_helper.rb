033c87b3c9cf3e0bcb2f508fec994e5a0d34b77b
macker-jp <masaya.fujiki@icloud.com>
2025-03-14 04:03:37 +0000

    Add @current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,9 +11,14 @@ module SessionsHelper
   end
 
   def current_user
-    return unless session[:user_id]
-
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif cookies.encrypted[:user_id]
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
