f99bcba473c4c49067aa8ffe0d7ffa9a634a27c0
macker-jp <masaya.fujiki@icloud.com>
2025-03-14 03:38:45 +0000

    Add remember user to create_action

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # 永続的セッションのためにユーザーをデータベースに記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   def current_user
     return unless session[:user_id]
 
