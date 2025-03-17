c5f7d0f4faa24c2a77f5d740eb6ed5cef7afb078
macker-jp <masaya.fujiki@icloud.com>
2025-03-17 02:58:05 +0000

    ログイン済みのユーザーがほかのユーザーを更新できないようにした。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: %i[edit update]
+  before_action :correct_user, only: %i[edit update]
   def show
     @user = User.find(params[:id])
   end
@@ -20,12 +21,9 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = 'Profile updated'
       redirect_to @user
@@ -47,5 +45,10 @@ class UsersController < ApplicationController
 
     flash[:danger] = 'Please log in'
     redirect_to login_url, status: :see_other
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
   end
 end
